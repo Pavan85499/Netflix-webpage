09ec033ca8ce80869739135be7ca6102a8f54c9a
Pavan Kumar V <pavansv87@gmail.com>
2024-10-24 19:45:57 +0530

    code

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
+from flask import Flask 
+from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Secret key for session management
@@ -6,7 +10,7 @@ app.secret_key = 'your_secret_key'  # Secret key for session management
 users = {'admin': '123'} # Dummy user data for demonstration
 
 
-@app.route('/login')
+@app.route('/')
 def retry():
     return render_template('login.html')
 
@@ -18,8 +22,20 @@ def index():
 def userprofile():
     return render_template('userprofile.html')
 
-@app.route('/signup')
-def signup():
+@app.route('/signIn', methods=['GET', 'POST'])
+def signIn():
+    if request.method == 'POST':
+        data = request.get_json()
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        confirm_password = data['confirm_password']
+
+
+        cursor = Mysql.connection.cursor()
+        cursor.execute('INSERT INTO USERS (Name, Email, Password, confirm_password) VALUES (%s,%s,%s)',(name,email,password,confirm_password))
+        Mysql.connent.commit()
+        cursor.close()
     return render_template('signIn.html')
 
 
@@ -40,14 +56,21 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/logout')
-def logout():
-    # Remove the user from the session (log out)
-    session.pop('username', None)
-    flash('You have been logged out.', 'info')
-    return redirect(url_for('login'))
+# ------------------DataBase---------------
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flask_database'
 
 
+# Create an instance of SQLAlchemy
+Mysql = MySQL(app)
+
+
+
+
+# Create the database tables
 
 if __name__ == "__main__":
     app.run(debug=True)
